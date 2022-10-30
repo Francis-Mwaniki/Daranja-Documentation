94b21d856f04d66d01f34906adeab645bba933c5
Francis-Mwaniki <francismwaniki630@gmail.com>
2022-10-30 15:59:42 +0300

    daranja Documentation

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,65 @@
-<template></template>
+<template>
+  <div
+    class="flex flex-col justify-center mx-auto items-center dark:bg-slate-800 bg-slate-200"
+  >
+    <strong
+      ><h1
+        class="dark:text-white md:text-3xl text-xl py-3 border-b-2 border-dotted dark:border-white border-black text-gray-600 font-extrabold leading-7 tracking-wider"
+      >
+        BACKEND USING NODE.JS
+      </h1></strong
+    >
+    <h2 class="dark:text-white text-gray-600 mt-2">Folder structure</h2>
+    <div class="flex justify-center items-center mx-auto md:flex-col flex-col">
+      <strong class="font-thin dark:text-white text-gray-600 border-b-2 border-red-500"
+        >Your folder structure</strong
+      >
+      <img src="../assets/img/folder.png" alt="" />
+    </div>
+    <h2 class="dark:text-white text-gray-600">Package json</h2>
+    <div class="flex justify-center items-center mx-auto md:flex-col flex-col">
+      <strong class="font-thin dark:text-white text-gray-600 border-b-2 border-red-500"
+        >Install all packages</strong
+      >
+      <img src="../assets/img/package.svg" alt="" />
+    </div>
+    <h2 class="dark:text-white text-gray-600">Main Index.js</h2>
+    <div class="flex justify-center items-center mx-auto md:flex-col flex-col">
+      <strong class="font-thin dark:text-white text-gray-600 border-b-2 border-red-500"
+        >Main app ->index.js requires.</strong
+      >
+      <img src="../assets/img/requires.svg" alt="" />
+    </div>
+    <h2 class="dark:text-white text-gray-600">Main Index.js</h2>
+    <div class="flex justify-center items-center mx-auto md:flex-col flex-col">
+      <strong class="font-thin dark:text-white text-gray-600 border-b-2 border-red-500"
+        >DB connection and middlewares.</strong
+      >
+      <img src="../assets/img/dbconnect.svg" alt="" />
+    </div>
+    <h2 class="dark:text-white text-gray-600">Main Index.js</h2>
+    <div class="flex justify-center items-center mx-auto md:flex-col flex-col">
+      <strong class="font-thin dark:text-white text-gray-600 border-b-2 border-red-500"
+        >Get Accesstoken and Send a register url to safaricom.</strong
+      >
+      <img src="../assets/img/getReq.svg" alt="" />
+    </div>
+    <h2 class="dark:text-white text-gray-600">Main Index.js</h2>
+    <div class="flex justify-center items-center mx-auto md:flex-col flex-col">
+      <strong class="font-thin dark:text-white text-gray-600 border-b-2 border-red-500"
+        >Simulate actions.</strong
+      >
+      <img src="../assets/img/simulate.svg" alt="" />
+    </div>
+    <h2 class="dark:text-white text-gray-600">Main Index.js</h2>
+    <div class="flex justify-center items-center mx-auto md:flex-col flex-col">
+      <strong class="font-thin dark:text-white text-gray-600 border-b-2 border-red-500"
+        >Push an stk to the registered User!!.</strong
+      >
+      <img src="../assets/img/stk.svg" alt="" />
+    </div>
+  </div>
+</template>
 
 <script>
 export default {};
